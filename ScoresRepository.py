470e98237903cd3722de0cd1c6ec3e9d6b7c491a
Mihaiull <mihail.ghilencea@stud.ubbcluj.ro>
2023-10-31 18:43:28 +0200

    documentation and more asserts

--- ScoresRepository.py
+++ ScoresRepository.py
@@ -8,47 +8,68 @@ class ScoresRepository:
         self.scores_undo = []
 
     # Add a score to the list
+    #@input: score - the score to be added
     def add_score(self, score):
         self.scores.append(score)
         self.scores_undo.append(self.scores[:])
+
+    #My refusal to use a for led me to use a for
+    #@input scores - a list of scores to be added
     def add_scores(self, scores):
         for i in range(len(scores)):
             self.scores.append(scores[i])
         self.scores_undo.append(self.scores[:])
+
     #Add a score at an index to the list
+    #@input: index - the index where the score will be added
+    #@input: score - the score to be added
     def add_score_at_index(self, index, score):
         self.scores.insert(index, score)
         self.scores_undo.append(self.scores[:])
 
     #Remove the score at an index:
+    #@input index - the index of the score to be removed
     def remove_score_at_index(self, index):
         self.scores.pop(index)
         self.scores_undo.append(self.scores[:])
+
     #Remove the scores between two indexes:
+    #@input index1 - the start index
+    #@input index2 - the end index
     def remove_scores_between_indexes(self, index1, index2):
         for i in range(index2, index1-1, -1):
             self.scores.pop(i)
         self.scores_undo.append(self.scores[:])
+
     #Replace the score at given index:
+    #@input index - the index of the score to be replaced
+    #@input score - the new score
     def replace_score_at_index(self, index, score):
         self.scores[index] = score
         self.scores_undo.append(self.scores[:])
     
     #Get indexes with scores less than a value:
+    #@input score - the value
+    #@output indexes - the list of indexes with scores less than the value
     def get_indexes_with_scores_less_than(self, score):
         indexes = []
         for i in range(len(self.scores)):
             if self.scores[i] < score:
                 indexes.append(i)
         return indexes
+    
     #Get all indexes sorted by their score:
+    #@output indexes - the list of indexes sorted by score
     def get_indexes_sorted_by_score(self):
         indexes = []
         for i in range(len(self.scores)):
             indexes.append(i)
         indexes.sort(key=lambda x: self.scores[x])
         return indexes
+    
     #Get indexes with scores higher than a value and sorted:
+    #@input score - the value
+    #@output indexes - the list of indexes with scores higher than the value
     def get_indexes_with_scores_higher_than(self, score):
         indexes = []
         for i in range(len(self.scores)):
@@ -58,12 +79,19 @@ class ScoresRepository:
         return indexes
     
     #Get the average scores between two indexes:
+    #@input index1 - the start index
+    #@input index2 - the end index
+    #@output average - the average of the scores between the indexes
     def get_average_scores_between_indexes(self, index1, index2):
         total = 0
         for i in range(index1, index2+1):
             total += self.scores[i]
-        return round(total / (index2 - index1), 2)
+        return round(total / (index2 - index1 + 1), 2)
+    
     #Get the minimum score between two indexes:
+    #@input index1 - the start index
+    #@input index2 - the end index
+    #@output minimum - the minimum score between the indexes
     def get_minimum_score_between_indexes(self, index1, index2):
         minimum = self.scores[index1]
         for i in range(index1, index2):
@@ -72,6 +100,9 @@ class ScoresRepository:
         return minimum
     
     #Get the scores of participants between two indexes that are multiples of a given value:
+    #@input index1 - the start index
+    #@input index2 - the end index
+    #@input multiple - the value
     def get_scores_between_indexes_multiples_of(self, index1, index2, multiple):
         scores = []
         for i in range(index1, index2):
@@ -80,6 +111,7 @@ class ScoresRepository:
         return scores
     
     #Keep only participants with scores multiple of a value, removing the others:
+    #@input multiple - the value
     def keep_only_scores_multiple_of(self, multiple):
         for i in range(len(self.scores)-1, -1, -1):
             if self.scores[i] % multiple != 0 or self.scores[i] < multiple or self.scores[i] == 0:
@@ -87,11 +119,13 @@ class ScoresRepository:
         self.scores_undo.append(self.scores[:])
 
     #Keep only participants with scores higher than a value, removing the other:
+    #@input score - the value
     def keep_only_scores_higher_than(self, score):
         for i in range(len(self.scores)-1, -1, -1):
             if self.scores[i] <= score:
                 del self.scores[i]
         self.scores_undo.append(self.scores[:])
+
     #UNDO:
     def undo(self):
         if len(self.scores_undo) == 0:
@@ -105,6 +139,8 @@ class ScoresRepository:
             except:
                 print("Nothing to undo!")
 
+    #Method to print the list of scores
+    #@output: string - the string to be printed
     def __str__(self):
         string = "\n"
         string +="[ "
--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ def test_add():
     assert repo.scores == [10] , "Should be [10], instead is " + str(repo.scores) + " (add failed)"
     repo.add_score(20)
     assert repo.scores == [10, 20] , "Should be [10, 20], instead is " + str(repo.scores) + " (add failed)" 
+    repo.add_score(30)
+    assert repo.scores == [10, 20, 30] , "Should be [10, 20, 30], instead is " + str(repo.scores) + " (add failed)"
 
 def test_add_at_index():
     repo = ScoresRepository()
@@ -14,6 +16,10 @@ def test_add_at_index():
     repo.add_score(30)
     repo.add_score_at_index(1, 15)
     assert repo.scores == [10, 15, 20, 30] , "Should be [10, 15, 20, 30], instead is " + str(repo.scores) + " (add_at_index failed)"
+    repo.add_score_at_index(0, 5)
+    assert repo.scores == [5, 10, 15, 20, 30] , "Should be [5, 10, 15, 20, 30], instead is " + str(repo.scores) + " (add_at_index failed)"
+    repo.add_score_at_index(5, 35)
+    assert repo.scores == [5, 10, 15, 20, 30, 35] , "Should be [5, 10, 15, 20, 30, 35], instead is " + str(repo.scores) + " (add_at_index failed)"
 
 def test_remove_at_index():
     repo = ScoresRepository()
@@ -22,68 +28,119 @@ def test_remove_at_index():
     repo.add_score(30)
     repo.remove_score_at_index(1)
     assert repo.scores == [10, 30] , "Should be [10, 30], instead is " + str(repo.scores) + " (remove_at_index failed)" 
+    repo.remove_score_at_index(0)
+    assert repo.scores == [30] , "Should be [30], instead is " + str(repo.scores) + " (remove_at_index failed)"
+    repo.remove_score_at_index(0)
+    assert repo.scores == [] , "Should be [], instead is " + str(repo.scores) + " (remove_at_index failed)"
 
 def test_remove_between_indexes():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     repo.remove_scores_between_indexes(1, 4)
     assert repo.scores == [10, 60] , "Should be [10, 60], instead is " + str(repo.scores) + " (remove_between_indexes failed)"
+    repo.remove_scores_between_indexes(0, 1)
+    assert repo.scores == [] , "Should be [], instead is " + str(repo.scores) + " (remove_between_indexes failed)"
+    repo.add_scores([10, 20, 30, 40, 50, 60])
+    repo.remove_scores_between_indexes(0, 5)
+    assert repo.scores == [] , "Should be [], instead is " + str(repo.scores) + " (remove_between_indexes failed)"
 
 def test_replace_at_index():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     repo.replace_score_at_index(1, 15)
     assert repo.scores == [10, 15, 30, 40, 50, 60] , "Should be [10, 15, 30, 40, 50, 60], instead is " + str(repo.scores) + " (replace_at_index failed)"
+    repo.replace_score_at_index(0, 5)
+    assert repo.scores == [5, 15, 30, 40, 50, 60] , "Should be [5, 15, 30, 40, 50, 60], instead is " + str(repo.scores) + " (replace_at_index failed)"
+    repo.replace_score_at_index(5, 65)
+    assert repo.scores == [5, 15, 30, 40, 50, 65] , "Should be [5, 15, 30, 40, 50, 65], instead is " + str(repo.scores) + " (replace_at_index failed)"
 
 def test_get_indexes_with_scores_less_than():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     assert repo.get_indexes_with_scores_less_than(30) == [0, 1] , "Should be [0, 1], instead is " + str(repo.get_indexes_with_scores_less_than(30)) + " (get_indexes_with_scores_less_than failed)"
+    assert repo.get_indexes_with_scores_less_than(35) == [0, 1, 2] , "Should be [0, 1, 2], instead is " + str(repo.get_indexes_with_scores_less_than(35)) + " (get_indexes_with_scores_less_than failed)"
+    assert repo.get_indexes_with_scores_less_than(40) == [0, 1, 2] , "Should be [0, 1, 2], instead is " + str(repo.get_indexes_with_scores_less_than(40)) + " (get_indexes_with_scores_less_than failed)"
 
 def test_get_indexes_sorted_by_score():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 40, 30, 50, 60])
     assert repo.get_indexes_sorted_by_score() == [0, 1, 3, 2, 4, 5] , "Should be [0, 1, 3, 2, 4, 5], instead is " + str(repo.get_indexes_sorted_by_score()) + " (get_indexes_sorted_by_score failed)"
+    repo.scores.clear()
+    repo.add_scores([10, 20, 30, 40, 60, 50])
+    assert repo.get_indexes_sorted_by_score() == [0, 1, 2, 3, 5, 4] , "Should be [0, 1, 2, 3, 5, 4], instead is " + str(repo.get_indexes_sorted_by_score()) + " (get_indexes_sorted_by_score failed)"
+    repo.scores.clear()
+    repo.add_scores([60, 50, 40, 30, 20, 10])
+    assert repo.get_indexes_sorted_by_score() == [5, 4, 3, 2, 1, 0] , "Should be [5, 4, 3, 2, 1, 0], instead is " + str(repo.get_indexes_sorted_by_score()) + " (get_indexes_sorted_by_score failed)"
+
 
 def test_get_indexes_with_scores_higher_than():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     assert repo.get_indexes_with_scores_higher_than(30) == [3, 4, 5] , "Should be [3, 4, 5], instead is " + str(repo.get_indexes_with_scores_higher_than(30)) + " (get_indexes_with_scores_higher_than failed)"
+    assert repo.get_indexes_with_scores_higher_than(35) == [3, 4, 5] , "Should be [3, 4, 5], instead is " + str(repo.get_indexes_with_scores_higher_than(35)) + " (get_indexes_with_scores_higher_than failed)"
+    assert repo.get_indexes_with_scores_higher_than(40) == [4, 5] , "Should be [4, 5], instead is " + str(repo.get_indexes_with_scores_higher_than(40)) + " (get_indexes_with_scores_higher_than failed)"
 
 def test_get_average_scores_between_indexes():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
-    assert repo.get_average_scores_between_indexes(1, 4) == 46.67 , "Should be 46.67, instead is " + str(repo.get_average_scores_between_indexes(1, 4)) + " (get_average_scores_between_indexes failed)"
+    assert repo.get_average_scores_between_indexes(1, 4) == 35 , "Should be 35, instead is " + str(repo.get_average_scores_between_indexes(1, 4)) + " (get_average_scores_between_indexes failed)"
+    assert repo.get_average_scores_between_indexes(1, 5) == 40 , "Should be 40, instead is " + str(repo.get_average_scores_between_indexes(1, 5)) + " (get_average_scores_between_indexes failed)"
+    assert repo.get_average_scores_between_indexes(0, 5) == 35 , "Should be 35, instead is " + str(repo.get_average_scores_between_indexes(0, 5)) + " (get_average_scores_between_indexes failed)"
 
 def test_get_minimum_score_between_indexes():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     assert repo.get_minimum_score_between_indexes(1, 4) == 20, "Should be 20, instead is " + str(repo.get_minimum_score_between_indexes(1, 4)) + " (get_minimum_score_between_indexes failed)"
+    assert repo.get_minimum_score_between_indexes(1, 5) == 20, "Should be 20, instead is " + str(repo.get_minimum_score_between_indexes(1, 5)) + " (get_minimum_score_between_indexes failed)"
+    assert repo.get_minimum_score_between_indexes(0, 5) == 10, "Should be 10, instead is " + str(repo.get_minimum_score_between_indexes(0, 5)) + " (get_minimum_score_between_indexes failed)"
 
 def test_get_scores_between_indexes_multiples_of():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     assert repo.get_scores_between_indexes_multiples_of(1, 4, 20) == [20, 40], "Should be [20, 40], instead is " + str(repo.get_scores_between_indexes_multiples_of(1, 4, 20)) + " (get_scores_between_indexes_multiples_of failed)"
+    assert repo.get_scores_between_indexes_multiples_of(1, 6, 20) == [20, 40, 60], "Should be [20, 40, 60], instead is " + str(repo.get_scores_between_indexes_multiples_of(1, 5, 20)) + " (get_scores_between_indexes_multiples_of failed)"
+    assert repo.get_scores_between_indexes_multiples_of(0, 6, 20) == [20, 40, 60], "Should be [20, 40, 60], instead is " + str(repo.get_scores_between_indexes_multiples_of(0, 5, 20)) + " (get_scores_between_indexes_multiples_of failed)"
 
 def test_keep_only_scores_multiple_of():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     repo.keep_only_scores_multiple_of(20)
     assert repo.scores == [20, 40, 60], "Should be [20, 40, 60], instead is " + str(repo.scores) + " (keep_only_scores_multiple_of failed)"
+    repo.scores.clear()
+    repo.add_scores([10, 20, 30, 40, 50, 60])
+    repo.keep_only_scores_multiple_of(10)
+    assert repo.scores == [10, 20, 30, 40, 50, 60], "Should be [10, 20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (keep_only_scores_multiple_of failed)"
+    repo.scores.clear()
+    repo.add_scores([10, 20, 30, 40, 50, 60])
+    repo.keep_only_scores_multiple_of(30)
+    assert repo.scores == [30, 60], "Should be [30, 60], instead is " + str(repo.scores) + " (keep_only_scores_multiple_of failed)"
 
 def test_keep_only_scores_higher_than():
     repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     repo.keep_only_scores_higher_than(30)
     assert repo.scores == [40, 50, 60] , "Should be [40, 50, 60], instead is " + str(repo.scores) + " (keep_only_scores_higher_than failed)"
+    repo.scores.clear()
+    repo.add_scores([10, 20, 30, 40, 50, 60])
+    repo.keep_only_scores_higher_than(10)
+    assert repo.scores == [20, 30, 40, 50, 60] , "Should be [20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (keep_only_scores_higher_than failed)"
+    repo.scores.clear()
+    repo.add_scores([10, 20, 30, 40, 50, 60])
+    repo.keep_only_scores_higher_than(60)
+    assert repo.scores == [] , "Should be [], instead is " + str(repo.scores) + " (keep_only_scores_higher_than failed)"
 
 def test_undo():
-    '''repo = ScoresRepository()
+    repo = ScoresRepository()
     repo.add_scores([10, 20, 30, 40, 50, 60])
     repo.remove_scores_between_indexes(1, 4)
     repo.undo()
-    assert repo.scores == [10, 20, 30, 40, 50, 60] , "Should be [10, 20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (undo failed)"'''
-    pass
+    assert repo.scores == [10, 20, 30, 40, 50, 60] , "Should be [10, 20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (undo failed)"
+    repo.remove_scores_between_indexes(0, 1)
+    repo.undo()
+    assert repo.scores == [10, 20, 30, 40, 50, 60] , "Should be [10, 20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (undo failed)"
+    repo.remove_scores_between_indexes(0, 5)
+    repo.undo()
+    assert repo.scores == [10, 20, 30, 40, 50, 60] , "Should be [10, 20, 30, 40, 50, 60], instead is " + str(repo.scores) + " (undo failed)"
 
 def test_all():
     string = ""
